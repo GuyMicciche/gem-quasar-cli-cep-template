a0a6581c24c07eac8feabbe67ac37734c7f487b5
Guy Micciche <king.guy@outlook.com>
2023-06-06 13:49:59 -0600

    Add files via upload

--- src/components/panel/Drawer.vue
+++ src/components/panel/Drawer.vue
@@ -2,7 +2,7 @@
   <q-drawer :value="leftDrawerOpen" show-if-above bordered>
     <q-list>
       <q-item-label header>Essential Links</q-item-label>
-      <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+      <q-item clickable tag="a" @click.native="openURL('https://quasar.dev')">
         <q-item-section avatar>
           <q-icon name="school" />
         </q-item-section>
@@ -11,12 +11,7 @@
           <q-item-label caption>quasar.dev</q-item-label>
         </q-item-section>
       </q-item>
-      <q-item
-        clickable
-        tag="a"
-        target="_blank"
-        href="https://github.quasar.dev"
-      >
+      <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
         <q-item-section avatar>
           <q-icon name="code" />
         </q-item-section>
@@ -43,12 +38,7 @@
           <q-item-label caption>forum.quasar.dev</q-item-label>
         </q-item-section>
       </q-item>
-      <q-item
-        clickable
-        tag="a"
-        target="_blank"
-        href="https://twitter.quasar.dev"
-      >
+      <q-item clickable tag="a" target="_blank" href="https://twitter.quasar.dev">
         <q-item-section avatar>
           <q-icon name="rss_feed" />
         </q-item-section>
@@ -57,12 +47,7 @@
           <q-item-label caption>@quasarframework</q-item-label>
         </q-item-section>
       </q-item>
-      <q-item
-        clickable
-        tag="a"
-        target="_blank"
-        href="https://facebook.quasar.dev"
-      >
+      <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
         <q-item-section avatar>
           <q-icon name="public" />
         </q-item-section>
@@ -77,9 +62,24 @@
 
 <script>
 export default {
+  computed: {
+    app() {
+      return this.$root.$children[0];
+    },
+  },
+  methods: {
+    openURL(url) {
+      try {
+        cep.util.openURLInDefaultBrowser(url);
+      } catch (error) {
+        alert("Could not open URL: " + error);
+        console.error("Could not open URL:", error);
+      }
+    },
+  },
   props: {
-    leftDrawerOpen: Boolean
-  }
+    leftDrawerOpen: Boolean,
+  },
 };
 </script>
 
